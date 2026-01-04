fe860e50715aed4af5a70eb70048b131d6f5160f
Devis <denis.savishchev@gmail.com>
2026-01-04 15:31:00 +0100

    first

---
+++ lib/choose_path_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChoosePathScreen extends StatelessWidget {
+  const ChoosePathScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+          onPressed: (){},
+          child: Text('Press')
+      ),
+    );
+  }
+}
