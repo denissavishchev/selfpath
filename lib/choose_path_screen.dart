ebb7d15f7baa14a382de9522d5bc496bc3e2fc7f
Devis <denis.savishchev@gmail.com>
2026-01-05 10:08:38 +0100

    languages, constants,

--- lib/choose_path_screen.dart
+++ lib/choose_path_screen.dart
@@ -1,14 +1,49 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:selfpath/constants.dart';
+import 'package:selfpath/widgets/flag_widget.dart';
 
 class ChoosePathScreen extends StatelessWidget {
   const ChoosePathScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-          onPressed: (){},
-          child: Text('Test')
+    Size size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      backgroundColor: kWhite,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            spacing: 12,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('language'.tr(),style: kTextStyle,),
+                  GestureDetector(
+                      onTap: () => context.setLocale(const Locale('en', 'US')),
+                      child: const FlagWidget(country: 'GB')),
+                  GestureDetector(
+                      onTap:  () => context.setLocale(const Locale('pl', 'PL')),
+                      child: const FlagWidget(country: 'PL')),
+                  GestureDetector(
+                      onTap: () => context.setLocale(const Locale('ru', 'RU')),
+                      child: const FlagWidget(country: 'RU')),
+                ],
+              ),
+              Container(
+                width: size.width,
+                height: size.height * 0.75,
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: kGreyDark,
+                  borderRadius: BorderRadius.all(Radius.circular(12))
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:selfpath/choose_path_screen.dart';
 import 'package:selfpath/providers/choose_path_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -13,19 +16,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: [
-          ChangeNotifierProvider<ChoosePathProvider>(create: (_) => ChoosePathProvider()),
-        ],
-        builder: (context, child) {
-          return ScreenUtilInit(
-            designSize: const Size(720, 1560),
-            builder: (_, child) => MaterialApp(
-              debugShowCheckedModeBanner: false,
-              home: ChoosePathScreen(),
-            ),
-          );
-        }
+    return EasyLocalization(
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('pl', 'PL'),
+        Locale('ru', 'RU'),],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en', 'US'),
+      child: MultiProvider(
+          providers: [
+            ChangeNotifierProvider<ChoosePathProvider>(create: (_) => ChoosePathProvider()),
+          ],
+          builder: (context, child) {
+            return ScreenUtilInit(
+              designSize: const Size(720, 1560),
+              builder: (_, child) => MaterialApp(
+                debugShowCheckedModeBanner: false,
+                localizationsDelegates: context.localizationDelegates,
+                supportedLocales: context.supportedLocales,
+                locale: context.locale,
+                home: ChoosePathScreen(),
+              ),
+            );
+          }
+      ),
     );
   }
 }
